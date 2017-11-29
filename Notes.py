b399dd11403381a99161b53988ada6da88d96eb1
SalvadorOrnelas <34071877+SalvadorOrnelas@users.noreply.github.com>
2017-11-29 10:48:01 -0800

    added function 10/28/17

--- Notes.py
+++ Notes.py
@@ -1,1 +1,82 @@
-# this is working
+print("hello world")
+
+# Salvador Ornelas jr
+
+
+print(3 + 5)
+print(5 - 3)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 2)
+
+print()  # this makes a new line. In python 3.6, it looks like: print()
+print("see if you can figure this out")
+print(5 % 3)
+
+car_name = "salvador mobile"
+car_type = "Magic school bus"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s" % car_name)
+
+# Asking for input
+name = input("whats your name? ") # in python 3, it is just called input()
+print("hello %s." % name)
+
+age = input("how old are you?") # in python 3, it is just called input()
+print("%s?! wow you are so young !" % age)
+song = input("whats your favorite song") # in python 3, is just called input
+print("%s?! wow that song is lit !" % song)
+
+# functions
+
+
+def print_hw():
+    print("hello world")
+
+
+print_hw()
+print_hw()
+print_hw()
+
+
+def say_hi(name):
+    print("hello %s." % name)
+    print("enjoy your day.")
+
+
+say_hi("timmy turner")
+
+
+def print_age(name, age):
+    print("%s is $d years old." % (name, age))
+    age += 1  # age = age +1
+    print("next year they will be %d" % age)
+
+
+print_age("Timmy turner", 15)
+
+
+def f(x):
+    return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+# if statements
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >=80 :
+        return "B"
+    elif percentage >= 70 :
+        return "C"
+    elif percentage >= 60 :
+        return "D"
+    elif percentage >= 50 :
+        return "F"
